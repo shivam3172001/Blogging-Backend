7e97d2ab863c5ce72a447c9b5c629d085331a8ea
Shivam kumar choudhary <152508164+shivam3172001@users.noreply.github.com>
2024-07-24 21:26:12 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200'
-    //'https://loquacious-seahorse-614b70.netlify.app/'
+    'https://rainbow-custard-1c352b.netlify.app/'
 
 ];
 
